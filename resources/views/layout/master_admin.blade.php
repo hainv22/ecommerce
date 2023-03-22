14d8e46ac8a9f93c12e96b855169921ab26e7591
hari <laravel1998bn@gmail.com>
2023-03-22 12:24:25 +0700

    update hide sidebar-collapse

--- resources/views/layout/master_admin.blade.php
+++ resources/views/layout/master_admin.blade.php
@@ -47,7 +47,7 @@
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
-  <body class="skin-blue sidebar-mini sidebar-collapse">
+  <body class="hold-transition skin-blue sidebar-mini">
     <div class="wrapper">
       <header class="main-header">
         <!-- Logo -->
