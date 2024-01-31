9e2a759a6860c47496c5e3feabfa2243cbf996e5
hari <laravel1998bn@gmail.com>
2024-01-31 17:25:59 +0700

    add year 2024 index transaction

--- resources/views/admin/transaction/index.blade.php
+++ resources/views/admin/transaction/index.blade.php
@@ -58,6 +58,7 @@
                             <option value="">__ Năm __</option>
                             <option value="2022" {{ Request::get('year') == 2022 ? "selected='selected'" : "" }}>Năm 2022</option>
                             <option value="2023" {{ Request::get('year') == 2023 ? "selected='selected'" : "" }}>Năm 2023</option>
+                            <option value="2024" {{ Request::get('year') == 2024 ? "selected='selected'" : "" }}>Năm 2024</option>
                         </select>
                         <button type="submit" class="btn btn-success"><i class="fa fa-search"> </i> Search</button>
                     </form>
